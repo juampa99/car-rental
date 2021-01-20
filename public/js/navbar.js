bb3655294efcb6b029d8812fa56569429b7a397e
Juan Pablo Allende <juanpabloallende2018@gmail.com>
2021-01-20 11:58:19 -0300

    Agrega menu para la version movil

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,12 @@
 const burger = document.querySelector('.navbar-burger');
+const dropdown = document.querySelector('.my-dropdown');
 burger.onclick = () => {
-    if(burger.classList.contains('is-active'))
+    if(burger.classList.contains('is-active')) {
         burger.classList.remove('is-active');
-    else
+        dropdown.style.display = "none";
+    }
+    else {
         burger.classList.add('is-active');
+        dropdown.style.display = "block";
+    }
 }
